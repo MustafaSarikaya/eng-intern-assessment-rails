4e7bf4da1e44c153eab3d4f17f4c62a2e7af4eb1
MustafaSarikaya <msarikay@uottawa.ca>
2025-07-13 18:33:20 -0400

    created the model and controller classes

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+
+    def self.search(queryText)
+        where('title LIKE :text OR content LIKE :text', (text: "%#{queryText}%"))
+    end
+end
+
+
+
